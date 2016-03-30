42922d8d4e96ecb076079c86f13874528f148732
Julien Dargelos <julien.dargelos@me.com>
2016-03-30 15:01:31 +0200

    SEO Optimizations

--- js/buy.js
+++ js/buy.js
@@ -1,3 +1,5 @@
+if(window.location.href.match(/^https?:\/\/d157rqmxrxj6ey\.cloudfront\.net\/bonneaudargelos\/42561/)) window.location.href='https://thimbleprojects.org/bonneaudargelos/42561/buy.html';
+
 var init=[
 	'scroll',
 	'client',
